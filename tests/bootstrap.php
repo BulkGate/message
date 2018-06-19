7b2c5820b2d83c5c6a1cb0019305e4f61b7df2b4
Lukáš Piják <lukaspijak@gmail.com>
2018-06-19 10:17:19 +0200

    git ignore

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@
 // invoked through the command: ../vendor/bin/tester .
 
 
-if (!require __DIR__ . '/../../../../vendor/autoload.php') {
+if (@!require __DIR__ . '/../vendor/autoload.php') {
 	echo 'Install Nette Tester using `composer install`';
 	exit(1);
 }
