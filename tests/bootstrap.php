01c850c1d5e225604e76b44d63b61d4385de1e20
Lukáš Piják <lukaspijak@gmail.com>
2018-06-19 10:17:19 +0200

    test bootstrap update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 // The Nette Tester command-line runner can be
 // invoked through the command: ../vendor/bin/tester .
 
-if (!require __DIR__ . '/../../../../vendor/autoload.php') {
+if (@!require __DIR__ . '/../vendor/autoload.php') {
 	echo 'Install Nette Tester using `composer install`';
 	exit(1);
 }
